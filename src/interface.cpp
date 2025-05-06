ca80e086ff68fcc6b80c82b19e476a09ae5140dd
= <tayx361@gmail.com>
2025-05-06 18:01:22 +0200

    no more cmake

--- src/interface.cpp
+++ src/interface.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include "player.h"
 
-// Definizioni delle funzioni
 void fill_board(char tavola[3][3]) {
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++) {
